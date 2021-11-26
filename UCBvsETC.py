705a057b63821dccbc821853aa66e884a5e1ef9f
KartikIITB <51525313+KartikIITB@users.noreply.github.com>
2021-11-26 07:42:58 +0530

    More changes. Added pickle database storage.

--- UCBvsETC.py
+++ UCBvsETC.py
@@ -1,6 +1,7 @@
 from math import sqrt, log, ceil
 
 import numpy as np
+import pickle
 import matplotlib.pyplot as plt
 from bandit_models.Algorithms import ETCAlgorithm, UCB1Algorithm
 from bandit_models.bandits import GaussianBandit
@@ -38,14 +39,20 @@ for Delta in Delta_values:
     bandit, Deltas = initializer(k, np.asarray([0, -Delta]), np.asarray([1, 1]))
     optimal_m = ETCAlgorithm(n, 100, bandit).optimal_m(Delta)
 
-    R_UCB[i], _ = UCB1Algorithm(n, bandit, delta).regret_analysis(1000, Deltas)
-    R_ETC_optimal[i], _ = ETCAlgorithm(n, optimal_m, bandit).regret_analysis(1000, Deltas)
-    R_ETC_25[i], _ = ETCAlgorithm(n, 25, bandit).regret_analysis(1000, Deltas)
-    R_ETC_50[i], _ = ETCAlgorithm(n, 50, bandit).regret_analysis(1000, Deltas)
-    R_ETC_75[i], _ = ETCAlgorithm(n, 75, bandit).regret_analysis(1000, Deltas)
-    R_ETC_100[i], _ = ETCAlgorithm(n, 100, bandit).regret_analysis(1000, Deltas)
+    R_UCB[i], _ = UCB1Algorithm(n, bandit, delta).regret_analysis(2000, Deltas)
+    R_ETC_optimal[i], _ = ETCAlgorithm(n, optimal_m, bandit).regret_analysis(2000, Deltas)
+    R_ETC_25[i], _ = ETCAlgorithm(n, 25, bandit).regret_analysis(2000, Deltas)
+    R_ETC_50[i], _ = ETCAlgorithm(n, 50, bandit).regret_analysis(2000, Deltas)
+    R_ETC_75[i], _ = ETCAlgorithm(n, 75, bandit).regret_analysis(2000, Deltas)
+    R_ETC_100[i], _ = ETCAlgorithm(n, 100, bandit).regret_analysis(2000, Deltas)
     print(R_UCB[i])
     i += 1
+
+dataset = {'UCB': R_UCB, 'ETC_optimal': R_ETC_optimal, 'm = 25': R_ETC_25, 'm = 50': R_ETC_50, 'm = 75': R_ETC_75, 'm = 100': R_ETC_100}
+DataFile = 'R_values.data'
+fw = open(DataFile, 'wb')
+pickle.dump(dataset, fw)
+fw.close()
 
 # Plotting and comparing results:
 plt.plot(Delta_values, R_UCB, marker = 'o')
@@ -58,6 +65,6 @@ plt.plot(Delta_values, R_ETC_100, marker = 'o')
 plt.title("$R_n$ for Various Values of $\Delta$")
 plt.xlabel('Values of Delta')
 plt.ylabel('Regret Values')
-plt.legend(['UBC', 'ETC, optimal', 'ETC, m = 25', 'ETC, m = 50', 'ETC, m = 75', 'ETC, m = 100'])
+plt.legend(['UCB', 'ETC, optimal', 'ETC, m = 25', 'ETC, m = 50', 'ETC, m = 75', 'ETC, m = 100'])
 plt.grid()
 plt.show()
